e3573e8fb73d513887def147d63049a9784f368f
Everton <everton@atttransbordo.local>
2023-06-21 16:20:28 -0300

    index for campaings

--- index.php
+++ index.php
@@ -3,17 +3,26 @@
 $ROOT_PATH = '.';
 require_once("{$ROOT_PATH}/assets/site_things.php");
 
-$account;
+/* $account;
 if (!$account) {
     Redirect('login/');
-}
+} */
 
-$index = new Site('Home');
+$index = new Site('Minhas campanhas');
 ?>
 
 <?php $index->Start() ?>
 
+<header class="bg-dark">
+    <h1 class="text-light">Minhas campanhas</h1>
+</header>
+
 <main class="d-flex justify-content-center align-items-center min-vh-100 w-100 bg-dark">
+    <div id="cartao-campanha" class="bg-light">
+        <h1>A volta dos que não foram</h1>
+        <p>Uma campanha sobre não sei o que</p>
+        <a href="">Isso é um link para entrar na campanha</a>
+    </div>
 </main>
 <script src="js/main.js"></script>
 
